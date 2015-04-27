a5c64eb250f41b25e25d250bfb549374c8343cd8
kylemcco <kyle.p.mccormick@gmail.com>
2015-04-27 14:17:03 -0500

    year to_s mostly done, white space issue

--- lib/year.rb
+++ lib/year.rb
@@ -1,3 +1,5 @@
+require_relative 'month'
+
 class Year
   attr_reader :year
 
@@ -18,12 +20,25 @@ class Year
   end
 
   def to_s
-    <<EOS
-#{year}
-Su Mo Tu We Th Fr Sa
+    year_output = ""
+    year_output << "#{year.to_s.center(63).rstrip}\n"
 
+    this_array = []
+    12.times {|month| this_array.push(Month.new((month + 1), @year)) }
+
+    this_array.each_slice(3) do |month|
+      year_output << <<EOS
+#{month[0].name.center(20)}  #{month[1].name.center(20)}  #{month[2].name.center(20).rstrip}
+#{month[0].print_days.lines.first.chomp}  #{month[1].print_days.lines.first.chomp}  #{month[2].print_days.lines.first.chomp}
+#{month[0].print_days.lines[1].chomp}  #{month[1].print_days.lines[1].chomp}  #{month[2].print_days.lines[1].chomp}
+#{month[0].print_days.lines[2].chomp}  #{month[1].print_days.lines[2].chomp}  #{month[2].print_days.lines[2].chomp}
+#{month[0].print_days.lines[3].chomp}  #{month[1].print_days.lines[3].chomp}  #{month[2].print_days.lines[3].chomp}
+#{month[0].print_days.lines[4].chomp}  #{month[1].print_days.lines[4].chomp}  #{month[2].print_days.lines[4].chomp}
+#{month[0].print_days.lines[5].chomp}  #{month[1].print_days.lines[5].chomp}  #{month[2].print_days.lines[5].chomp}
 
 EOS
+    end
+    print year_output
   end
 
 end
