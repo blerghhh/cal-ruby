3dc3ecb511d036e3e26d7ab05992b80d6d2a7679
kylemcco <kyle.p.mccormick@gmail.com>
2015-04-27 15:34:00 -0500

    working on whitespace issues for Year

--- lib/year.rb
+++ lib/year.rb
@@ -21,7 +21,7 @@ class Year
 
   def to_s
     year_output = ""
-    year_output << "#{year.to_s.center(63).rstrip}\n"
+    year_output << "#{year.to_s.center(63).rstrip}\n\n"
 
     this_array = []
     12.times {|month| this_array.push(Month.new((month + 1), @year)) }
@@ -29,15 +29,26 @@ class Year
     this_array.each_slice(3) do |month|
       year_output << <<EOS
 #{month[0].name.center(20)}  #{month[1].name.center(20)}  #{month[2].name.center(20).rstrip}
-#{month[0].print_days.lines.first.chomp}  #{month[1].print_days.lines.first.chomp}  #{month[2].print_days.lines.first.chomp}
+#{month[0].print_days.lines[0].chomp}  #{month[1].print_days.lines[0].chomp}  #{month[2].print_days.lines[0].chomp}
 #{month[0].print_days.lines[1].chomp}  #{month[1].print_days.lines[1].chomp}  #{month[2].print_days.lines[1].chomp}
 #{month[0].print_days.lines[2].chomp}  #{month[1].print_days.lines[2].chomp}  #{month[2].print_days.lines[2].chomp}
 #{month[0].print_days.lines[3].chomp}  #{month[1].print_days.lines[3].chomp}  #{month[2].print_days.lines[3].chomp}
 #{month[0].print_days.lines[4].chomp}  #{month[1].print_days.lines[4].chomp}  #{month[2].print_days.lines[4].chomp}
-#{month[0].print_days.lines[5].chomp}  #{month[1].print_days.lines[5].chomp}  #{month[2].print_days.lines[5].chomp}
-
+#{month[0].print_days.lines[5].ljust(20).chomp}  #{month[1].print_days.lines[5].ljust(20).chomp}  #{month[2].print_days.lines[5].ljust(20).chomp}
 EOS
-    end
+
+      month.each do |single|
+        if !single.print_days.lines[6].nil?
+          year_output << "#{single.print_days.lines[6].ljust(20).chomp}"
+        else
+          year_output << "#{(" " * 22).chomp}"
+        end
+
+      end #end of month.each
+      year_output << "\n"
+
+    end # end of each_slice
+
     print year_output
   end
 
