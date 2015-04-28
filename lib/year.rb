806bf11c874b9bde047bd1f80cfa795bc799e0b6
kylemcco <kyle.p.mccormick@gmail.com>
2015-04-28 14:47:57 -0500

    year to_s refactor

--- lib/year.rb
+++ lib/year.rb
@@ -24,36 +24,26 @@ class Year
     year_arr    = []
     12.times {|month| year_arr.push(Month.new((month + 1), @year)) }
     year_output << "#{year.to_s.center(63).rstrip}\n\n"
-    year_arr.each_slice(3) do |month|
+    year_arr.each_slice(3) do |month_row|
       year_output << <<EOS
-#{month[0].name.center(20)}  #{month[1].name.center(20)}  #{month[2].name.center(20).rstrip}
+#{month_row[0].name.center(20)}  #{month_row[1].name.center(20)}  #{month_row[2].name.center(20).rstrip}
 EOS
-      line_num = 0
-      while line_num < 7
-        month.each do |single|
-          if !single.print_days.lines[line_num].nil?
-            if single.print_days.lines[line_num] == single.print_days.lines.last
-              if single != month.last
-                year_output << "#{single.print_days.lines[line_num].chomp.ljust(22)}"
+      7.times do |n|
+        month_row.each do |month|
+          if !month.print_days.lines[n].nil?
+              if month != month_row.last
+                year_output << "#{month.print_days.lines[n].chomp.ljust(22)}"
               else
-                  year_output << "#{single.print_days.lines[line_num].chomp.ljust(22).rstrip}\n"
+                year_output << "#{month.print_days.lines[n].rstrip}\n"
               end
-            else
-              if single != month.last
-                year_output << "#{single.print_days.lines[line_num].chomp.ljust(22)}"
-              else
-                year_output << "#{single.print_days.lines[line_num].chomp.ljust(22).rstrip}\n"
-              end
-            end
           else
-            if single != month.last
+            if month != month_row.last
               year_output << "#{(" " * 22).chomp.ljust(22)}"
             else
               year_output << "\n"
             end
           end
         end
-        line_num += 1
       end
     end
     year_output
