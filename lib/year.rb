fa30289bb416e3374ab22aaa5760f2c5d4a16c98
kylemcco <kyle.p.mccormick@gmail.com>
2015-04-28 14:01:30 -0500

    refactored year to_s and added more unit tests

--- lib/year.rb
+++ lib/year.rb
@@ -21,35 +21,41 @@ class Year
 
   def to_s
     year_output = ""
+    year_arr    = []
+    12.times {|month| year_arr.push(Month.new((month + 1), @year)) }
     year_output << "#{year.to_s.center(63).rstrip}\n\n"
-
-    this_array = []
-    12.times {|month| this_array.push(Month.new((month + 1), @year)) }
-
-    this_array.each_slice(3) do |month|
+    year_arr.each_slice(3) do |month|
       year_output << <<EOS
 #{month[0].name.center(20)}  #{month[1].name.center(20)}  #{month[2].name.center(20).rstrip}
-#{month[0].print_days.lines[0].chomp}  #{month[1].print_days.lines[0].chomp}  #{month[2].print_days.lines[0].chomp}
-#{month[0].print_days.lines[1].chomp}  #{month[1].print_days.lines[1].chomp}  #{month[2].print_days.lines[1].chomp}
-#{month[0].print_days.lines[2].chomp}  #{month[1].print_days.lines[2].chomp}  #{month[2].print_days.lines[2].chomp}
-#{month[0].print_days.lines[3].chomp}  #{month[1].print_days.lines[3].chomp}  #{month[2].print_days.lines[3].chomp}
-#{month[0].print_days.lines[4].chomp}  #{month[1].print_days.lines[4].chomp}  #{month[2].print_days.lines[4].chomp}
-#{month[0].print_days.lines[5].ljust(20).chomp}  #{month[1].print_days.lines[5].ljust(20).chomp}  #{month[2].print_days.lines[5].ljust(20).chomp}
 EOS
-
-      month.each do |single|
-        if !single.print_days.lines[6].nil?
-          year_output << "#{single.print_days.lines[6].ljust(20).chomp}"
-        else
-          year_output << "#{(" " * 22).chomp}"
+      line_num = 0
+      while line_num < 7
+        month.each do |single|
+          if !single.print_days.lines[line_num].nil?
+            if single.print_days.lines[line_num] == single.print_days.lines.last
+              if single != month.last
+                year_output << "#{single.print_days.lines[line_num].chomp.ljust(22)}"
+              else
+                  year_output << "#{single.print_days.lines[line_num].chomp.ljust(22).rstrip}\n"
+              end
+            else
+              if single != month.last
+                year_output << "#{single.print_days.lines[line_num].chomp.ljust(22)}"
+              else
+                year_output << "#{single.print_days.lines[line_num].chomp.ljust(22).rstrip}\n"
+              end
+            end
+          else
+            if single != month.last
+              year_output << "#{(" " * 22).chomp.ljust(22)}"
+            else
+              year_output << "\n"
+            end
+          end
         end
-
-      end #end of month.each
-      year_output << "\n"
-
-    end # end of each_slice
-
-    print year_output
+        line_num += 1
+      end
+    end
+    year_output
   end
-
 end
--- test/test_year.rb
+++ test/test_year.rb
@@ -28,7 +28,7 @@ class TestYear < Minitest::Test
     assert_equal 29, y.leap?
   end
 
-  def test_month_august
+  def test_year_2012
   output = `./cal.rb 2012`
   expected = <<EOS
                              2012
@@ -70,4 +70,91 @@ EOS
   assert_equal expected, output
   end
 
+  def test_year_3000
+  output = `./cal.rb 3000`
+  expected = <<EOS
+                             3000
+
+      January               February               March
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+          1  2  3  4                     1                     1
+ 5  6  7  8  9 10 11   2  3  4  5  6  7  8   2  3  4  5  6  7  8
+12 13 14 15 16 17 18   9 10 11 12 13 14 15   9 10 11 12 13 14 15
+19 20 21 22 23 24 25  16 17 18 19 20 21 22  16 17 18 19 20 21 22
+26 27 28 29 30 31     23 24 25 26 27 28     23 24 25 26 27 28 29
+                                            30 31
+       April                  May                   June
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+       1  2  3  4  5               1  2  3   1  2  3  4  5  6  7
+ 6  7  8  9 10 11 12   4  5  6  7  8  9 10   8  9 10 11 12 13 14
+13 14 15 16 17 18 19  11 12 13 14 15 16 17  15 16 17 18 19 20 21
+20 21 22 23 24 25 26  18 19 20 21 22 23 24  22 23 24 25 26 27 28
+27 28 29 30           25 26 27 28 29 30 31  29 30
+                                            
+        July                 August              September
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+       1  2  3  4  5                  1  2      1  2  3  4  5  6
+ 6  7  8  9 10 11 12   3  4  5  6  7  8  9   7  8  9 10 11 12 13
+13 14 15 16 17 18 19  10 11 12 13 14 15 16  14 15 16 17 18 19 20
+20 21 22 23 24 25 26  17 18 19 20 21 22 23  21 22 23 24 25 26 27
+27 28 29 30 31        24 25 26 27 28 29 30  28 29 30
+                      31                    
+      October               November              December
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+          1  2  3  4                     1      1  2  3  4  5  6
+ 5  6  7  8  9 10 11   2  3  4  5  6  7  8   7  8  9 10 11 12 13
+12 13 14 15 16 17 18   9 10 11 12 13 14 15  14 15 16 17 18 19 20
+19 20 21 22 23 24 25  16 17 18 19 20 21 22  21 22 23 24 25 26 27
+26 27 28 29 30 31     23 24 25 26 27 28 29  28 29 30 31
+                      30                    
+EOS
+
+  assert_equal expected, output
+  end
+
+  def test_year_1801
+  output = `./cal.rb 1801`
+  expected = <<EOS
+                             1801
+
+      January               February               March
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+             1  2  3   1  2  3  4  5  6  7   1  2  3  4  5  6  7
+ 4  5  6  7  8  9 10   8  9 10 11 12 13 14   8  9 10 11 12 13 14
+11 12 13 14 15 16 17  15 16 17 18 19 20 21  15 16 17 18 19 20 21
+18 19 20 21 22 23 24  22 23 24 25 26 27 28  22 23 24 25 26 27 28
+25 26 27 28 29 30 31                        29 30 31
+                                            
+       April                  May                   June
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+          1  2  3  4                  1  2      1  2  3  4  5  6
+ 5  6  7  8  9 10 11   3  4  5  6  7  8  9   7  8  9 10 11 12 13
+12 13 14 15 16 17 18  10 11 12 13 14 15 16  14 15 16 17 18 19 20
+19 20 21 22 23 24 25  17 18 19 20 21 22 23  21 22 23 24 25 26 27
+26 27 28 29 30        24 25 26 27 28 29 30  28 29 30
+                      31                    
+        July                 August              September
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+          1  2  3  4                     1         1  2  3  4  5
+ 5  6  7  8  9 10 11   2  3  4  5  6  7  8   6  7  8  9 10 11 12
+12 13 14 15 16 17 18   9 10 11 12 13 14 15  13 14 15 16 17 18 19
+19 20 21 22 23 24 25  16 17 18 19 20 21 22  20 21 22 23 24 25 26
+26 27 28 29 30 31     23 24 25 26 27 28 29  27 28 29 30
+                      30 31                 
+      October               November              December
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+             1  2  3   1  2  3  4  5  6  7         1  2  3  4  5
+ 4  5  6  7  8  9 10   8  9 10 11 12 13 14   6  7  8  9 10 11 12
+11 12 13 14 15 16 17  15 16 17 18 19 20 21  13 14 15 16 17 18 19
+18 19 20 21 22 23 24  22 23 24 25 26 27 28  20 21 22 23 24 25 26
+25 26 27 28 29 30 31  29 30                 27 28 29 30 31
+                                            
+EOS
+
+  assert_equal expected, output
+  end
+
+
 end
+
+
